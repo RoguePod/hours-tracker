5de871071d5396626ce106329e1bb1c0e204677a
Jeff Harper <jeffharper@roguepod.com>
2019-04-14 08:19:13 -0600

    fix wrapping problem for recents

--- src/javascripts/app/containers/recents-list/RecentRow.js
+++ src/javascripts/app/containers/recents-list/RecentRow.js
@@ -3,14 +3,6 @@ import PropTypes from "javascripts/prop-types";
 import React from "react";
 import _get from "lodash/get";
 import _isEqual from "lodash/isEqual";
-import styled from "styled-components";
-
-const Ellipsis = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  width: 100%;
-`;
 
 class RecentRow extends React.Component {
   static propTypes = {
@@ -55,20 +47,20 @@ class RecentRow extends React.Component {
     return (
       <div className="border-b border-grey p-4">
         <div className="flex items-center">
-          <div className="flex-1 pr-4">
-            <Ellipsis>
-              <em>
-                <small>{recent.client.name}</small>
-              </em>
-            </Ellipsis>
-            <Ellipsis>{recent.project.name}</Ellipsis>
+          <div className="flex-1">
+            <em>
+              <small>{recent.client.name}</small>
+            </em>
+            <div>{recent.project.name}</div>
           </div>
-          <ActionIcon
-            icon="play"
-            onClick={this._handleStart}
-            title="Start"
-            type="button"
-          />
+          <div className="pl-4">
+            <ActionIcon
+              icon="play"
+              onClick={this._handleStart}
+              title="Start"
+              type="button"
+            />
+          </div>
         </div>
       </div>
     );
